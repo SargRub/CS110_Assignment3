38684d9e7a08d7842a65455bec6b71034e8916c2
SargRub <rubensargsyan854@gmail.com>
2022-11-09 22:36:51 +0400

    A slight change in the Exercise 4's solution.

--- Ex4.js
+++ Ex4.js
@@ -2,6 +2,6 @@ const arr = [1, 2, 3];
 const reversed_arr = [];
 for(let i = 0; i < arr.length; i++)
 {
-    reversed_arr[i] = arr[2-i];
+    reversed_arr[i] = arr[arr.length-1-i];
 }
 console.log(reversed_arr);
